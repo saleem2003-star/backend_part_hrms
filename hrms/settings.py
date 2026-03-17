6c944da024c8b75a2873cfcf1c7fc79c01973afb
vanipenta saleem <vanipentashaiksaleem@gmail.com>
2026-03-17 15:40:41 +0530

    Update settings.py

--- hrms/settings.py
+++ hrms/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'PORT': '3306',
         'CONN_MAX_AGE': 300,  # 🔥 VERY IMPORTANT
         'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
         }
     }
